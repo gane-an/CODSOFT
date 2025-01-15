6bd5c6884d41318a67b7d28ecf9f61dd352fe562
gane-an <9843ganesan@gmail.com>
2025-01-15 21:03:30 +0530

    Update main.js

--- Portfolio/assets/js/main.js
+++ Portfolio/assets/js/main.js
@@ -1,4 +1,4 @@
-alert(" This site includes sample content only, Thank you!")
+alert(" This site includes sample content only.\n Thank you!")
 /* ----- NAVIGATION BAR FUNCTION ----- */
 const menuBtn = document.getElementById("myNavMenu");
 const closebtn = document.querySelector(".nav-menu-btn");
